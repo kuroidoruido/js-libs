e10efe141896c307a1ef5ec6a98da36985e44284
Anthony Pena <anthony.pena@outlook.fr>
2024-10-03 01:02:25 +0200

    feat(libs/fp): improve deepClone

--- libs/fp/src/clone.ts
+++ libs/fp/src/clone.ts
@@ -1,3 +1,5 @@
+import { isNotDefined } from './predicates';
+
 export function deepClone<T>(x: T): T {
   if (x instanceof Date) {
     return new Date(x.getTime()) as T;
@@ -5,11 +7,26 @@ export function deepClone<T>(x: T): T {
   if (typeof x === 'bigint') {
     return BigInt(x.toString()) as T;
   }
-  if (typeof x === 'undefined') {
+  if (
+    isNotDefined(x) ||
+    typeof x === 'boolean' ||
+    typeof x === 'number' ||
+    x === Number.NEGATIVE_INFINITY ||
+    x === Number.POSITIVE_INFINITY
+  ) {
     return x;
   }
-  if (x === Number.NEGATIVE_INFINITY || x === Number.POSITIVE_INFINITY) {
-    return x;
+  if (Array.isArray(x)) {
+    return x.map(deepClone) as T;
+  }
+  if (typeof x === 'object') {
+    return Object.fromEntries(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      Object.entries(x as any).map(([k, v]): [typeof k, typeof v] => [
+        k,
+        deepClone(v),
+      ]),
+    ) as T;
   }
   return JSON.parse(JSON.stringify(x));
 }
