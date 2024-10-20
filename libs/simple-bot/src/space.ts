894e9b9509df9e6b3bdd72dc03fdb827b555ac0a
Anthony Pena <anthony.pena@outlook.fr>
2024-10-20 18:31:16 +0200

    refactor(libs/simple-bot): bot space is now a class
    - BREAKING: bots are now passed after creating the space
    - env is processed automatically to decode base64 values
    - bot can be given as BotCreator to build it from config (like decoded env)

--- libs/simple-bot/src/space.ts
+++ libs/simple-bot/src/space.ts
@@ -1,27 +1,53 @@
+import { isDefined } from '@anthonypena/fp';
 import { BaseContext, Bot } from './bot';
 import { printInConsole } from './emitters/print-in-console';
+import { env } from './env';
 
 export interface SpaceConfig {
   env: Record<string, string>;
 }
 
-export function createBotSpace<T extends Record<string, unknown>>(
-  bots: Bot<T>[],
-  { env }: SpaceConfig,
-) {
-  return {
-    async run() {
-      for (const bot of bots) {
-        const baseContext: BaseContext = { botName: bot.name, env };
-        if (await bot.trigger(baseContext)) {
-          console.log(`[${bot.name}] Start`);
+export interface BotBuilder<
+  TFetchedData extends Record<string, unknown> = Record<string, never>,
+> {
+  (config: SpaceConfig): Bot<TFetchedData>;
+}
 
-          console.log(`[${bot.name}] Start data fetching`);
-          const data = await bot.data(baseContext);
-          console.log(`[${bot.name}] End data fetching`);
-          console.log(`[${bot.name}] Start building message`);
-          const message = await bot.message({ ...baseContext, data });
-          console.log(`[${bot.name}] End building message`);
+export class BotSpace {
+  private bots: Bot<Record<string, unknown>>[] = [];
+
+  constructor(private config: SpaceConfig) {
+    this.config.env = env(this.config.env);
+  }
+
+  addBots(
+    ...bots: (
+      | Bot<Record<string, unknown>>
+      | BotBuilder<Record<string, unknown>>
+    )[]
+  ) {
+    this.bots.push(
+      ...bots.map((bot) =>
+        typeof bot === 'function' ? bot(this.config) : bot,
+      ),
+    );
+    return this;
+  }
+
+  async run() {
+    const { env } = this.config;
+    for (const bot of this.bots) {
+      const baseContext: BaseContext = { botName: bot.name, env };
+      if (await bot.trigger(baseContext)) {
+        console.log(`[${bot.name}] Start`);
+
+        console.log(`[${bot.name}] Start data fetching`);
+        const data = await bot.data(baseContext);
+        console.log(`[${bot.name}] End data fetching`);
+        console.log(`[${bot.name}] Start building message`);
+        const message = await bot.message({ ...baseContext, data });
+        console.log(`[${bot.name}] End building message`);
+        if (isDefined(message)) {
           if (env.DRY === 'true') {
             console.log(`[${bot.name}] Start sending message (DRY=true)`);
             await printInConsole()({ ...baseContext, message });
@@ -31,12 +57,14 @@ export function createBotSpace<T extends Record<string, unknown>>(
             await bot.emitter({ ...baseContext, message });
             console.log(`[${bot.name}] End sending message`);
           }
-
-          console.log(`[${bot.name}] End`);
         } else {
-          console.log(`[${bot.name}] Will not run`);
+          console.log(`[${bot.name}] No message to send`);
         }
+
+        console.log(`[${bot.name}] End`);
+      } else {
+        console.log(`[${bot.name}] Will not run`);
       }
-    },
-  };
+    }
+  }
 }
